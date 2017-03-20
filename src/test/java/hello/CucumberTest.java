f869c6f03300f54b4e1f72343507816d62566a3d
Paula Tuñón Alba <paula.tualba@gmail.com>
2017-03-20 10:49:42 +0100

    Added a cucumber dependency to solve build errors

--- src/test/java/hello/CucumberTest.java
+++ src/test/java/hello/CucumberTest.java
@@ -3,7 +3,7 @@ package hello;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
-import cucumber.*;
+import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features")

1a0cebf8e551b0da635627c9615332326570f0da
guille <guille10000@gmail.com>
2017-03-29 21:45:38 +0200

    Code cleanup
    Signed-off-by: guille <guille10000@gmail.com>

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -5,12 +5,6 @@ import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @Controller
 public class MainController {
